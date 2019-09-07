c22c2e1dc97d6bff1097cc16fdaae7c12b3c05c1
eren <eren@192.168.0.53>
2019-09-07 16:22:17 +0200

    added more tests

--- src/test/java/TranslatorTest.java
+++ src/test/java/TranslatorTest.java
@@ -3,10 +3,21 @@
 
 package pack;
 
+import java.util.ArrayList;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 /**
  *
  * @author kenny
  */
 public class TranslatorTest {
 
+    ArrayList<String> answerList = new ArrayList<>();
+    
+    @Test
+    public void test1(){
+        answerList.add("test");
+        assertEquals(answerList.size(), 1);
+    }
 }
