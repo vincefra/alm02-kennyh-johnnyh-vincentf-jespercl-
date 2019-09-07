cd603060ab698e9695f439598411311fdaafc9e2
eren <eren@192.168.0.53>
2019-09-08 00:13:54 +0200

    added more tests

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -15,12 +15,18 @@ import java.util.logging.Logger;
  */
 public class PersonTest 
 {
-    Person person = new Person("Eren","Yeager","titan");
+    Person person = new Person("Eren","Yeager","Titan");
     public static final Logger LOG = Logger.getLogger(PersonTest.class.getName());
     
     @Test
     public void test1(){
         LOG.log(Level.INFO, "Testing the method of adding data in object, String Eren :{0}", person.getName());
-        assertEquals("Eren",person.getName());
+        assertEquals("Eren", person.getName());
+        
+        LOG.log(Level.INFO, "Testing the method of getGender", person.getGender());
+        assertEquals("Titan", person.getGender());
+        
+        LOG.log(Level.INFO, "Testing the method of getLastname", person.getLastname());
+        assertEquals("Yeager", person.getLastname());
     }
 }
